23f08bc1f24c9f5e0c493afb45f6ecf2973f1902
jackzzs <jackzzs@outlook.com>
2025-07-06 00:13:32 +0800

    Bump version: 7.4.0 → 7.4.1

--- hf/app.py
+++ hf/app.py
@@ -14,7 +14,7 @@ import socketio
 import eventlet
 import requests
 
-EK_VERSION = "7.4.0"
+EK_VERSION = "7.4.1"
 APP_DATA_DIR = Path(user_data_dir("embykeeper"))
 VERSION_CACHE_DIR = APP_DATA_DIR / "hf" / "version"
 
