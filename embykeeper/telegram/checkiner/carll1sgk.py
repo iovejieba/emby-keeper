bf5626a542a740ac843b05640884f50493883375
iovejieba <iovejieba@qq.com>
2025-10-26 17:34:43 +0800

    Update carll1sgk.py

--- embykeeper/telegram/checkiner/carll1sgk.py
+++ embykeeper/telegram/checkiner/carll1sgk.py
@@ -5,5 +5,5 @@ __ignore__ = True
 
 class Carll1SGKCheckin(BotCheckin):
     name = "Carll 社工库 1"
-    bot_username = "Carllnet_bot"
-    bot_checkin_cmd = "/qd"
+    bot_username = "Kaernet_bot"
+    bot_checkin_cmd = "/sign"
