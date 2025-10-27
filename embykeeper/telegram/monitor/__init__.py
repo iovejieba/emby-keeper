2147e45525809cd824a81dcf3dcf94965b60a699
iovejieba <iovejieba@qq.com>
2025-10-27 12:25:24 +0800

    1

--- embykeeper/telegram/monitor/__init__.py
+++ embykeeper/telegram/monitor/__init__.py
@@ -5,13 +5,11 @@ if _t.TYPE_CHECKING:
     from ._base import Session, Monitor, UniqueUsername
     from ._templ_a import TemplateAMonitor, TemplateAMonitorConfig
     from ._templ_b import TemplateBMonitor, TemplateBMonitorConfig
-    from ._templ_c import TemplateCMonitor, TemplateCMonitorConfig  # 新增TemplateC
 
 MENU = {
     "._base": ["Session", "Monitor", "UniqueUsername"],
     "._templ_a": ["TemplateAMonitor", "TemplateAMonitorConfig"],
     "._templ_b": ["TemplateBMonitor", "TemplateBMonitorConfig"],
-    "._templ_c": ["TemplateCMonitor", "TemplateCMonitorConfig"],  # 新增TemplateC
 }
 
 
